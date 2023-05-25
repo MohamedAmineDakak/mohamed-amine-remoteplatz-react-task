38d18f7551c0f4fb1f0ccc2846f60c4768f876ed
Mohamed Amine Dakak <amine.dakak@gmail.com>
2023-05-25 11:15:46 +0100

    fetch data from the API : - add the necessary constants - add the state - add the function to fetch the data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const TICKER = "SPUS";
+const API_BASE_URL = "https://query1.finance.yahoo.com/v7/finance/download";
+
+const App = () => {
+  const [fromDate, setFromDate] = useState(1633381200);
+  const [toDate, setToDate] = useState(1664917199);
+  const [interval, setInterval] = useState("1d");
+
+  useEffect(() => {
+    fetchData();
+  }, [fromDate, toDate, interval]);
+
+  const fetchData = async () => {
+    try {
+      const url = `${API_BASE_URL}/${TICKER}?period1=${fromDate}&period2=${toDate}&interval=${interval}&events=history`;
+      const response = await fetch(url);
+      const data = await response.text();
+
+      console.log("Data:", data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  return <div>Loading...</div>;
+};
 
 export default App;
